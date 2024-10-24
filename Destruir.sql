65aaf2e0d6c76c5b0865d9e51152b5d15e351074
João Italo Deodato <166622223+Happjoi@users.noreply.github.com>
2024-10-24 09:46:04 -0300

    Update Destruir.sql

--- Destruir.sql
+++ Destruir.sql
@@ -1,17 +1,6 @@
 -- Drop views (se houver)
 DROP VIEW IF EXISTS view_name_1;
 DROP VIEW IF EXISTS view_name_2;
--- Adicionar mais views conforme necessário
-
--- Drop procedures (se houver)
-DROP PROCEDURE IF EXISTS procedure_name_1;
-DROP PROCEDURE IF EXISTS procedure_name_2;
--- Adicionar mais procedures conforme necessário
-
--- Drop functions (se houver)
-DROP FUNCTION IF EXISTS function_name_1;
-DROP FUNCTION IF EXISTS function_name_2;
--- Adicionar mais functions conforme necessário
 
 -- Drop tables in the correct order considering foreign key dependencies
 DROP TABLE IF EXISTS Indexacao;
@@ -26,6 +15,3 @@ DROP TABLE IF EXISTS Livros;
 DROP TABLE IF EXISTS Autores;
 DROP TABLE IF EXISTS Departamentos;
 
--- If there are sequences or other dependencies like triggers, you should also drop them (optional)
-DROP SEQUENCE IF EXISTS sequence_name;
-DROP TRIGGER IF EXISTS trigger_name;
